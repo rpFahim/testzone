77edb6c426e466a5cc2850395e73ee46cb82ee23
RageX-131 <fahimmuntachirsifat@gmail.com>
2026-08-13 01:05:42 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // উদাহরণ: https://script.google.com/macros/s/AKfycbxxxxxxxx/exec
 window.STUDENTS_FEES_CONFIG = {
   API_URL:
-    "https://script.google.com/macros/s/AKfycbwZySjxsXxN47805AWld4958_1YVm3-ek9anexyIzkn7h8e7leWOHU2fwp-YkjK-NePhg/exec",
+    "https://script.google.com/macros/s/AKfycbzEK61Qq_kAVAFiFusApdatdv5UFozqkM3U7pAW0hN8mbcJPGwMylQ95-PlezwXQmNXcA/exec",
 };
